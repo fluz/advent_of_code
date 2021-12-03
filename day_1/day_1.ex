47d8c943fb9dfd465014c06e3a1c94304c4443dc
Fernando Luz <fernando.luz@talkdesk.com>
2021-12-03 17:37:05 +0000

    day 1 - step 2

--- day_1/day_1.ex
+++ day_1/day_1.ex
@@ -1,16 +1,29 @@
 defmodule Day1 do
-    def main do
-      {:ok, contents} = File.read("input.txt")
-      cs = contents |> String.split("\n", trim: true )|> Enum.map(&String.to_integer/1)
-      countPrevIncrease(cs, 0)
-    end
 
-    def countPrevIncrease(numbers, count) do
-      case numbers do
-        [prev, curr | tail] ->
-          countPrevIncrease([curr | tail], count + (if prev < curr do 1 else 0 end))
-        _ -> IO.puts(count)
-      end
+  def main do
+    case File.read("input.txt") do
+      {:ok, contents} ->
+        contents
+        |> String.split("\n", trim: true )
+        |> Enum.map(&String.to_integer/1)
+        |> sumAvg([])
+        |> countPrevIncr(0)
+      {:error, :enoent} ->
+        IO.puts("Not able to open the file.")
     end
+  end
 
+  def sumAvg(ns, acc) do
+    case ns do
+    [prev, curr, next | tail] -> sumAvg([curr, next | tail],  [(prev + curr + next) | acc])
+    _ -> Enum.reverse(acc)
+    end
+  end
+
+  def countPrevIncr(ns, count) do
+    case ns do
+      [prev, curr | tail] -> countPrevIncr([curr | tail], count + (if prev < curr do 1 else 0 end))
+      _ -> IO.puts(count)
+    end
+  end
 end
