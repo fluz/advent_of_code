4063ef8fac14ed10f156da9e39ef0db7ded3c615
Fernando Luz <fernando.luz@talkdesk.com>
2021-12-06 10:23:28 +0000

    day 6 - step 1 and 2

--- 2021/day6/day6.ex
+++ 2021/day6/day6.ex
@@ -1,7 +1,11 @@
 defmodule Day6 do
 
   def main do
-    p0 = readInput("./day6/input_test.txt")
+    p0 = readInput("./day6/input.txt")
+
+    p0
+    |> updateGeneration(256)
+    |> sumPopulation()
   end
 
   def readInput(filename) do
@@ -15,6 +19,30 @@ defmodule Day6 do
       {:error, :enoent} ->
         IO.puts("Not able to open the file.")
     end
+  end
+
+  def updateGeneration(xs, d) do
+    case d do
+      0 -> xs
+      _ ->
+        newGen = %{}
+        xs
+        |> Enum.reduce(newGen, fn ({key,value}, acc) ->
+          case key do
+            0 ->
+              Map.update(acc, 6, value, fn c -> c + value end)
+              |> Map.update( 8, value, fn c -> c + value end)
+            k -> Map.update(acc, k-1, value, fn c -> c + value end)
+          end
+        end)
+        |> updateGeneration(d - 1)
+    end
+  end
+
+  # Helper function
+  def sumPopulation(xs) do
+    xs
+    |> Enum.reduce(0, fn({_k, v}, acc) -> v + acc end)
   end
 
 end
